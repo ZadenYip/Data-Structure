9340d3be244be61a24f3a7c97e2a775c1fd0087a
Zaden Yip <hyfrancn@gmail.com>
2024-11-27 23:34:22 +0800

    feature: add default constructor and assignment operator to Edge class

--- include/graph/edge.h
+++ include/graph/edge.h
@@ -1,3 +1,4 @@
+#pragma once
 namespace data_structures {
 class Edge {
    private:
@@ -6,11 +7,13 @@ class Edge {
     double weight_;
 
    public:
+    Edge();
     Edge(int from, int to, double weight);
     Edge(int from, int to);
     int GetFrom() const;
     int GetTo() const;
     double GetWeight() const;
     void SetWeight(double weight);
+    Edge& operator=(const Edge& edge);
 };
 }  // namespace data_structures
