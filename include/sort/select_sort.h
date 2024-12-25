4f5054f6eeaad7a9f3c8aed155e75e09819c4a4f
Zaden Yip <hyfrancn@gmail.com>
2024-12-25 08:25:47 +0800

    refactor: change parameter type from pointer to array in sort methods

--- include/sort/select_sort.h
+++ include/sort/select_sort.h
@@ -12,7 +12,7 @@ class SelectSort : public BaseSort<T> {
      */
     SelectSort(std::function<bool(const T&, const T&)> priority_comparator)
         : BaseSort<T>(priority_comparator) {}
-    void sort(T* data, int length) override {
+    void sort(T data[], int length) override {
         for (int i = 0; i < length; i++) {
             int min_index = i;
             for (int j = i; j < length; j++) {
